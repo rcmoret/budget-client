723802432f79328f0b55d4100d27a79ab1d6dbe4
Ryan Moret <ryancmoret@gmail.com>
2023-03-04 17:54:02 -0700

    Add index.js, App.js, App.css

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component} from "react";
+import "./App.css";
+
+export default App = () => {
+  return(
+    <div className="App">
+      <h1> Hello, World! </h1>
+    </div>
+  );
+};
